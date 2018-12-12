8408e112d4a14fcb69af039d3d3069c19d4a4688
Haoliang Yu <hyu@esri.com>
2018-12-11 20:39:52 -0500

    pretty code

--- test/src/model.test.js
+++ test/src/model.test.js
@@ -5,90 +5,103 @@
   correctly.
 */
 
-const test = require('tape')
-const proxyquire = require('proxyquire')
-const fetchMock = require('fetch-mock')
-const fs = require('fs')
-const path = require('path')
+const test = require("tape");
+const proxyquire = require("proxyquire");
+const fetchMock = require("fetch-mock");
+const fs = require("fs");
+const path = require("path");
 
-test('it should send a request for a URL', t => {
-  t.plan(3)
-
-  const config = {
-    'source': 'http://my-site.com/points.csv',
-    'column.x': 'longitude',
-    'column.y': 'latitude'
-  }
-
-  const csv = fs.readFileSync(path.join(__dirname, '../fixtures/points.csv'), 'utf-8')
-
-  const fetch = fetchMock
-    .sandbox()
-    .mock('http://my-site.com/points.csv', csv)
-
-  const Model = proxyquire('../../src/model', {
-    'node-fetch': fetch,
-    'config': {
-      get (path) {
-        return config[path]
-      }
-    }
-  })
-  const model = new Model()
-
-  model.getData({}, (err, geojson) => {
-    t.error(err, 'no error')
-    t.equal(geojson.type, 'FeatureCollection', 'creates a feature collection object')
-    t.ok(geojson.features, 'has features')
-  })
-})
-
-test('it should load the local file for a file path with .csv', t => {
-  t.plan(3)
+test("it should send a request for a URL", t => {
+  t.plan(3);
 
   const config = {
-    'source': path.join(__dirname, '../fixtures/points.csv'),
-    'column.x': 'longitude',
-    'column.y': 'latitude'
-  }
+    source: "http://my-site.com/points.csv",
+    "column.x": "longitude",
+    "column.y": "latitude"
+  };
 
-  const Model = proxyquire('../../src/model', {
-    'config': {
-      get (path) {
-        return config[path]
+  const csv = fs.readFileSync(
+    path.join(__dirname, "../fixtures/points.csv"),
+    "utf-8"
+  );
+
+  const fetch = fetchMock.sandbox().mock("http://my-site.com/points.csv", csv);
+
+  const Model = proxyquire("../../src/model", {
+    "node-fetch": fetch,
+    config: {
+      get(path) {
+        return config[path];
       }
     }
-  })
-  const model = new Model()
+  });
+  const model = new Model();
 
   model.getData({}, (err, geojson) => {
-    t.error(err, 'no error')
-    t.equal(geojson.type, 'FeatureCollection', 'creates a feature collection object')
-    t.ok(geojson.features, 'has features')
-  })
-})
+    t.error(err, "no error");
+    t.equal(
+      geojson.type,
+      "FeatureCollection",
+      "creates a feature collection object"
+    );
+    t.ok(geojson.features, "has features");
+  });
+});
 
-test('it should load the local file for a file path with .CSV', t => {
-  t.plan(3)
+test("it should load the local file for a file path with .csv", t => {
+  t.plan(3);
 
   const config = {
-    'source': path.join(__dirname, '../fixtures/points.CSV'),
-    'column.x': 'longitude',
-    'column.y': 'latitude'
-  }
+    source: path.join(__dirname, "../fixtures/points.csv"),
+    "column.x": "longitude",
+    "column.y": "latitude"
+  };
 
-  const Model = proxyquire('../../src/model', {
-    'config': {
-      get (path) {
-        return config[path]
+  const Model = proxyquire("../../src/model", {
+    config: {
+      get(path) {
+        return config[path];
       }
     }
-  })
-  const model = new Model()
+  });
+  const model = new Model();
 
   model.getData({}, (err, geojson) => {
-    t.error(err, 'no error')
-    t.equal(geojson.type, 'FeatureCollection', 'creates a feature collection object')
-    t.ok(geojson.features, 'has features')
-  })
-})
+    t.error(err, "no error");
+    t.equal(
+      geojson.type,
+      "FeatureCollection",
+      "creates a feature collection object"
+    );
+    t.ok(geojson.features, "has features");
+  });
+});
+
+test("it should load the local file for a file path with .CSV", t => {
+  t.plan(3);
+
+  const config = {
+    source: path.join(__dirname, "../fixtures/points.CSV"),
+    "column.x": "longitude",
+    "column.y": "latitude"
+  };
+
+  const Model = proxyquire("../../src/model", {
+    config: {
+      get(path) {
+        return config[path];
+      }
+    }
+  });
+  const model = new Model();
+
+  model.getData({}, (err, geojson) => {
+    t.error(err, "no error");
+    t.equal(
+      geojson.type,
+      "FeatureCollection",
+      "creates a feature collection object"
+    );
+    t.ok(geojson.features, "has features");
+  });
+});
