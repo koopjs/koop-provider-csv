c99429b951206b65a359a4ab670e6f1632939e1d
Haoliang Yu <haoliangyu@users.noreply.github.com>
2018-12-11 20:44:10 -0500

    S/config (#1)
    * use config instead of dotenv
    
    * pretty code

--- src/util/translate-csv.js
+++ src/util/translate-csv.js
@@ -1,50 +1,51 @@
-const splitLines = require('split-lines');
+const splitLines = require("split-lines");
 
-function translate (csv) {
-  const columnX = process.env.COLUMN_X
-  const columnY = process.env.COLUMN_Y
-  const delimiter = process.env.DELIMITER || ','
-  const rows = splitLines(csv.trim())
-  const columns =  rows[0].split(delimiter).map(formatString)
+function translate(csv, { columnX, columnY, delimiter = "," }) {
+  const rows = splitLines(csv.trim());
+  const columns = rows[0].split(delimiter).map(formatString);
 
   return {
-    type: 'FeatureCollection',
-    features: rows.slice(1).map((row) => formatFeature(row, columns, { x: columnX, y: columnY }, delimiter))
-  }
+    type: "FeatureCollection",
+    features: rows
+      .slice(1)
+      .map(row =>
+        formatFeature(row, columns, { x: columnX, y: columnY }, delimiter)
+      )
+  };
 }
 
-function formatString (value) {
+function formatString(value) {
   if (value.startsWith('"') && value.endsWith('"')) {
-    return value.slice(1, value.length - 1)
+    return value.slice(1, value.length - 1);
   }
 
-  return value
+  return value;
 }
 
-function formatFeature (row, columns, coordColumns, delimiter) {
+function formatFeature(row, columns, coordColumns, delimiter) {
   // Most of what we need to do here is extract the longitude and latitude
   const feature = {
-    type: 'Feature',
+    type: "Feature",
     properties: {},
     geometry: {
-      type: 'Point',
+      type: "Point",
       coordinates: []
     }
-  }
+  };
 
-  const values = row.split(delimiter).map(formatString)
+  const values = row.split(delimiter).map(formatString);
 
   for (let i = 0; i < columns.length; i++) {
     if (columns[i] === coordColumns.x) {
       feature.geometry.coordinates.unshift(parseFloat(values[i]));
     } else if (columns[i] === coordColumns.y) {
-      feature.geometry.coordinates.push(parseFloat(values[i]))
+      feature.geometry.coordinates.push(parseFloat(values[i]));
     } else {
       feature.properties[columns[i]] = values[i];
     }
   }
 
-  return feature
+  return feature;
 }
 
-module.exports = translate
+module.exports = translate;
--- test/src/model.test.js
+++ test/src/model.test.js
@@ -5,76 +5,103 @@
   correctly.
 */
 
-const test = require('tape')
-const proxyquire = require('proxyquire')
-const fetchMock = require('fetch-mock')
-const dotEnv = require('dotenv')
-const fs = require('fs')
-const path = require('path')
+const test = require("tape");
+const proxyquire = require("proxyquire");
+const fetchMock = require("fetch-mock");
+const fs = require("fs");
+const path = require("path");
 
-dotEnv.config({
-  path: path.join(__dirname, '../.env.test')
-})
+test("it should send a request for a URL", t => {
+  t.plan(3);
 
-test('it should send a request for a URL', t => {
-  t.plan(3)
+  const config = {
+    source: "http://my-site.com/points.csv",
+    "column.x": "longitude",
+    "column.y": "latitude"
+  };
 
-  dotEnv.config({
-    path: path.join(__dirname, '../.env.test')
-  })
+  const csv = fs.readFileSync(
+    path.join(__dirname, "../fixtures/points.csv"),
+    "utf-8"
+  );
 
-  const csv = fs.readFileSync(path.join(__dirname, '../fixtures/points.csv'), 'utf-8')
+  const fetch = fetchMock.sandbox().mock("http://my-site.com/points.csv", csv);
 
-  const fetch = fetchMock
-    .sandbox()
-    .mock('http://my-site.com/points.csv', csv)
-
-  const Model = proxyquire('../../src/model', {
-    'node-fetch': fetch
-  })
-  const model = new Model()
-
-  model.getData({}, (err, geojson) => {
-    t.error(err, 'no error')
-    t.equal(geojson.type, 'FeatureCollection', 'creates a feature collection object')
-    t.ok(geojson.features, 'has features')
-  })
-})
-
-test('it should load the local file for a file path with .csv', t => {
-  t.plan(3)
-
-  dotEnv.config({
-    path: path.join(__dirname, '../.env.test')
-  })
-
-  process.env.CSV_SOURCE = path.join(__dirname, '../fixtures/points.csv')
-
-  const Model = require('../../src/model')
-  const model = new Model()
+  const Model = proxyquire("../../src/model", {
+    "node-fetch": fetch,
+    config: {
+      get(path) {
+        return config[path];
+      }
+    }
+  });
+  const model = new Model();
 
   model.getData({}, (err, geojson) => {
-    t.error(err, 'no error')
-    t.equal(geojson.type, 'FeatureCollection', 'creates a feature collection object')
-    t.ok(geojson.features, 'has features')
-  })
-})
+    t.error(err, "no error");
+    t.equal(
+      geojson.type,
+      "FeatureCollection",
+      "creates a feature collection object"
+    );
+    t.ok(geojson.features, "has features");
+  });
+});
 
-test('it should load the local file for a file path with .CSV', t => {
-  t.plan(3)
+test("it should load the local file for a file path with .csv", t => {
+  t.plan(3);
 
-  dotEnv.config({
-    path: path.join(__dirname, '../.env.test')
-  })
+  const config = {
+    source: path.join(__dirname, "../fixtures/points.csv"),
+    "column.x": "longitude",
+    "column.y": "latitude"
+  };
 
-  process.env.CSV_SOURCE = path.join(__dirname, '../fixtures/points.CSV')
-
-  const Model = require('../../src/model')
-  const model = new Model()
+  const Model = proxyquire("../../src/model", {
+    config: {
+      get(path) {
+        return config[path];
+      }
+    }
+  });
+  const model = new Model();
 
   model.getData({}, (err, geojson) => {
-    t.error(err, 'no error')
-    t.equal(geojson.type, 'FeatureCollection', 'creates a feature collection object')
-    t.ok(geojson.features, 'has features')
-  })
-})
+    t.error(err, "no error");
+    t.equal(
+      geojson.type,
+      "FeatureCollection",
+      "creates a feature collection object"
+    );
+    t.ok(geojson.features, "has features");
+  });
+});
+
+test("it should load the local file for a file path with .CSV", t => {
+  t.plan(3);
+
+  const config = {
+    source: path.join(__dirname, "../fixtures/points.CSV"),
+    "column.x": "longitude",
+    "column.y": "latitude"
+  };
+
+  const Model = proxyquire("../../src/model", {
+    config: {
+      get(path) {
+        return config[path];
+      }
+    }
+  });
+  const model = new Model();
+
+  model.getData({}, (err, geojson) => {
+    t.error(err, "no error");
+    t.equal(
+      geojson.type,
+      "FeatureCollection",
+      "creates a feature collection object"
+    );
+    t.ok(geojson.features, "has features");
+  });
+});
