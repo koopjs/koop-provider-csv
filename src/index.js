20353c1c54346719bd8ac2c59cad96da904a432f
Haoliang Yu <haoliangyu@users.noreply.github.com>
2019-05-10 16:07:25 -0400

    F/make compatible with CLI (#6)

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@
   Documentation: http://koopjs.github.io/docs/specs/provider/
 */
 
-const config = require('config');
+const config = require("config")["koop-provider-csv"];
 
 // Define the provider path
 // /:name/:hosts?/:disableIdParam?/FeatureServer/:layer/:method
 // e.g. /sample/FeatureServer/0/query
 const provider = {
-  type: 'provider',
-  name: config.get('sourceName'),
+  type: "provider",
+  name: config.sourceName,
   hosts: false, // if true, also adds disableIdParam
   disableIdParam: true, // if true, adds to path and req.params
-  Model: require('./model'),
-  version: require('../package.json').version
-}
+  Model: require("./model"),
+  version: require("../package.json").version
+};
 
-module.exports = provider
+module.exports = provider;
